5c186aa48ea05d45dafe52261a6ece8728352e52
spettigrew <sara.pettigrew5@gmail.com>
2020-02-21 16:35:29 -0700

    Fixed typos

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-const replacelyForm = document.getElementById("replacely-form");
+const replaceyForm = document.getElementById("replacey-form");
 const searchText = document.getElementById("search");
 const replaceText = document.getElementById("replace");
 
-replacelyForm.onsubmit = function(e){
+replaceyForm.onsubmit = function(e){
     e.preventDefault();
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "REPLACE_TEXT", find: searchText.value, replace: replaceText.value});
